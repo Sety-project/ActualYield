dbea37b99286f22cac61db68c9f17a1efe1f7403
daviidarr <nononymous@gmail.com>
2024-03-18 21:17:46 +0100

    async

--- utils/db.py
+++ utils/db.py
@@ -7,7 +7,7 @@ from datetime import datetime, timezone
 import pandas as pd
 from sqlalchemy import Float, DateTime, Connection, Engine, String
 
-from utils.async_utils import async_wrap
+from utils.async_utils import async_wrap, safe_gather
 
 
 class CsvDB:
@@ -28,37 +28,36 @@ class CsvDB:
         if not os.path.isdir(self.data_dir):
             os.mkdir(self.data_dir)
             os.chmod(self.data_dir, 0o777)
-        
+
+    async def query_snapshot(self, address: str, timestamp: int) -> dict:
+        with open(os.path.join(self.data_dir, f'snapshot_{address}_{timestamp}.json'), 'r') as f:
+            return await async_wrap(json.load)(f)
+
     async def insert_snapshot(self, dict_result: dict, address: str) -> None:
         with open(os.path.join(os.sep, self.data_dir, f"snapshot_{address}_{int(dict_result['timestamp'])}.json"), 'w') as f:
             async_wrap(json.dump)(dict_result, f, ensure_ascii=False, indent=4)
 
-    def query_explain_data(self, address: str, start_date: datetime, end_date: datetime = datetime.now()) -> tuple[dict, dict, list]:
-        all_timestamps = [int(file) for file in os.listdir(self.data_dir)
-            if file.startswith('snapshot') and file.endswith('.json') and address in file]
-        start_timestamp = next((ts for ts in sorted(all_timestamps, reverse=True)
-                                if ts <= start_date.timestamp()), min(all_timestamps))
-        end_timestamp = next((ts for ts in sorted(all_timestamps, reverse=False)
-                              if ts >= end_date.timestamp()), max(all_timestamps))
+    async def query_explain_data(self, address: str, start_date: datetime, end_date: datetime = datetime.now()) -> tuple[dict, dict, list]:
+        timestamps = await self.all_timestamps(address)
+        start_timestamp = next((ts for ts in sorted(timestamps, reverse=True)
+                                if ts <= start_date.timestamp()), min(timestamps))
+        end_timestamp = next((ts for ts in sorted(timestamps, reverse=False)
+                              if ts >= end_date.timestamp()), max(timestamps))
 
-        with open(os.path.join(self.data_dir, f'snapshot_{start_timestamp}.json'), 'r') as f:
-            start_snapshot = json.load(f)
-        with open(os.path.join(self.data_dir, f'snapshot_{end_timestamp}.json'), 'r') as f:
-            end_snapshot = json.load(f)
-            
+        snapshots = await asyncio.gather(self.query_snapshot(address, start_timestamp),self.query_snapshot(address, end_timestamp))
+        start_snapshot, end_snapshot = snapshots[0], snapshots[1]
         transactions = []
         
         return start_snapshot, end_snapshot, transactions
 
-    async def last_updated(self, address: str) -> datetime:
-        await asyncio.sleep(0)
-        if all_timestamps := [
-            int(file.split('_')[2].split('.')[0])
-            for file in os.listdir(self.data_dir)
-            if file.startswith('snapshot')
-            and file.endswith('.json')
-            and address in file
-        ]:
-            return datetime.fromtimestamp(max(all_timestamps), tz=timezone.utc)
+    async def all_timestamps(self, address: str) -> list[int]:
+        return [int(file.split('_')[2].split('.')[0]) for file in os.listdir(self.data_dir)
+                if file.startswith('snapshot') and file.endswith('.json') and address in file]
+
+    async def last_updated(self, address: str) -> tuple[datetime, dict]:
+        if all_timestamps := await self.all_timestamps(address):
+            timestamp = max(all_timestamps)
+            latest_snapshot = await self.query_snapshot(address, timestamp)
+            return datetime.fromtimestamp(timestamp, tz=timezone.utc), latest_snapshot
         else:
-            return datetime(1970, 1, 1, tzinfo=timezone.utc)
+            return datetime(1970, 1, 1, tzinfo=timezone.utc), {}
