08943fa62aa814af546f157bf6529ae6d29e7332
daviidarr <nononymous@gmail.com>
2024-06-04 14:30:00 +0200

    actualyield docker

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 # syntax=docker/dockerfile:1
 
-FROM python:3.9-slim-bullseye
+FROM python:3.11-bullseye
 RUN apt-get update
 RUN DEBIAN_FRONTEND="noninteractive" apt-get install -y python3-pip
 RUN pip3 install --upgrade pip
--- run.sh
+++ run.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-cd ~/actualyield
-source ./venv/bin/activate
-
-git pull origin main
-pip install -r requirements.txt
-
-python3 ./cli.py snapshot >> ./cli.log
-
-# Deactivate the virtual environment
-deactivate
+python3 ./cli.py snapshot
